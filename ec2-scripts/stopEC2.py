1482c4c9a8e47033d0280ab7e993214441efc300
pankajvivanet <96774115+pankajvivanet@users.noreply.github.com>
2023-05-28 13:23:21 +0530

    Update stopEC2.py

--- ec2-scripts/stopEC2.py
+++ ec2-scripts/stopEC2.py
@@ -1,27 +1,33 @@
 import boto3
+import sys
 
-AWS_REGION = ["ap-south-1"]
+AWS_REGION = ["us-east-1","us-east-2"]
+
+aws_access_key_id=sys.argv[1]
+aws_secret_access_key=sys.argv[2]
 
 for region in AWS_REGION:
-  session = boto3.Session(profile_name='lab')
-  client = session.client("ec2",region_name=region)
-  instance_ids=[]
+    client = boto3.client("ec2", aws_access_key_id=aws_access_key_id,
+                       aws_secret_access_key=aws_secret_access_key, 
+                       region_name=region)
+    instance_ids=[]
+    
 
-  def get_ec2_details():
-    reservations = client.describe_instances()
+    def get_ec2_details():
+        reservations = client.describe_instances()
 
-    for reservation in reservations['Reservations']:
-        for instance in reservation['Instances']:
-            if instance['State']['Name'] == 'running':
-                instance_ids.append(instance['InstanceId'])
+        for reservation in reservations['Reservations']:
+            for instance in reservation['Instances']:
+                if instance['State']['Name'] == 'running':
+                    instance_ids.append(instance['InstanceId'])
+        
+        print(instance_ids)
+        return instance_ids
 
-    print(instance_ids)
-    return instance_ids
+    def stop_ec2_instances(instance_ids):
+        for ec2_id in instance_ids:
+            response = client.stop_instances(InstanceIds=[ec2_id])
+            print(response)
 
-  def stop_ec2_instances(instance_ids):
-    for ec2_id in instance_ids:
-        response = client.stop_instances(InstanceIds=[ec2_id])
-        print(response)
-
-  instance_ids = get_ec2_details()
-  stop_ec2_instances(instance_ids)    
+    instance_ids = get_ec2_details()
+    stop_ec2_instances(instance_ids)
