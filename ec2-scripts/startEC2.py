d9546dbf6ccfcf729cee1c191f158fc2a71fd5c4
pankajvivanet <96774115+pankajvivanet@users.noreply.github.com>
2023-05-29 07:57:38 +0530

    Create startEC2.py

---
+++ ec2-scripts/startEC2.py
@@ -0,0 +1,32 @@
+import boto3
+import sys
+
+AWS_REGION = ["us-east-1","us-east-2"]
+
+aws_access_key_id=sys.argv[1]
+aws_secret_access_key=sys.argv[2]
+
+for region in AWS_REGION:
+    client = boto3.client("ec2", aws_access_key_id=aws_access_key_id,
+                       aws_secret_access_key=aws_secret_access_key, 
+                       region_name=region)
+    instance_ids=[]
+
+    def get_ec2_details():
+        reservations = client.describe_instances()
+
+        for reservation in reservations['Reservations']:
+            for instance in reservation['Instances']:
+                if instance['State']['Name'] == 'stopped':
+                    instance_ids.append(instance['InstanceId'])
+        
+        print(instance_ids)
+        return instance_ids
+
+    def start_ec2_instances(instance_ids):
+        for ec2_id in instance_ids:
+            response = client.start_instances(InstanceIds=[ec2_id])
+            print(response)
+
+    instance_ids = get_ec2_details()
+    start_ec2_instances(instance_ids)
